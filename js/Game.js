402e4448b20e9389bec8b3bd83c9c0d0d4720926
shamita1512 <chemica1cooki3@gmail.com>
2020-07-05 13:56:15 +0530

    Multiple tabs, turn red when you are on a tab

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,29 @@ class Game {
       form.display();
     }
   }
+  play(){
+    form.hidden();
+    textSize(30);
+    text("Game Start",120,300);
+    player.fetchPlayerInfo();
+    if(allPlayers !== undefined){
+      var displayPos = 130;
+      for(var plr in allPlayers){
+        if(plr === "player" + player.index){
+          fill("red");
+          
+        }
+        else{
+          fill("black");
+        }
+        displayPos += 20;
+        textSize(15);
+        text(allPlayers[plr].name + ":" + allPlayers[plr].distance, 120,displayPos);
+      }
+    }
+    if(keyDown(UP_ARROW) && player.index !== null){
+      player.distance += 20;
+      player.update();
+    }
+  }
 }
